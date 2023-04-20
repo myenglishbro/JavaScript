255a04d21127900bd427ea177a063ea2e1e2c9bd
Carlos Apolaya <temis_it@hotmail.com>
2023-04-20 11:36:32 -0500

    Motores de plantillas + Websocket

--- SegundaEntrega/src/index.js
+++ SegundaEntrega/src/index.js
@@ -3,11 +3,12 @@ import productRouter from "./router/product.routes.js"
 import cartRouter from "./router/carts.routes.js"
 import handlebars from 'express-handlebars';
 import __dirname from './utils.js';
-
+import ProductManager from "./Managers/ProductManager.js";
 
 
 const app=express()
 const PORT=8080
+const product = new ProductManager();
 app.engine('handlebars', handlebars.engine());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
@@ -23,6 +24,14 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 // app.use(express.static(__dirname + '/public'))
 
+app.use("/api",async(req,res)=>{
+    let allProducts=await product.getProducts()
+    res.render("home",{
+        webtitle:"Coder",
+        products:allProducts
+
+    })
+})
 app.use("/api/products",productRouter)
 app.use("/api/carts",cartRouter)
 
