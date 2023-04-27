5108b4d04c227fcfe421149a42acb32f0d1578a7
Carlos Apolaya <temis_it@hotmail.com>
2023-04-27 13:19:32 -0500

    DESAFIO MEJORADO

--- SegundaEntrega/src/index.js
+++ SegundaEntrega/src/index.js
@@ -33,31 +33,62 @@ app.use('/', viewRouter);
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
 
-const server=app.listen(PORT,()=>{
-    console.log("Servidor Express Funcionando")
-})
+const server = app.listen(PORT, () => {
+    console.log("Servidor Express Funcionando");
+  });
+  
+  const product = new ProductManager();
+  const io = new Server(server);
+  
+  io.on('connection', async socket => {
+    console.log("cliente conectado");
+    const products = await product.getProducts();
+    socket.emit('productos', products);
+  
+    socket.on('addProduct', async data => {
+      console.log(data);
+      await product.addProduct(data);
+      const updatedProducts = await product.getProducts(); // Obtener la lista actualizada de productos
+      io.emit('productos', updatedProducts);
+    });
+  
+    socket.on('deleteProduct', async data => {
+      console.log("ID del producto a eliminar: ", data);
+      const deletedProduct = await product.deleteProduct(data);
+      console.log("Producto eliminado: ", deletedProduct);
+      const updatedProducts = await product.getProducts(); // Obtener la lista actualizada de productos
+      io.emit('productos', updatedProducts);
+    });
+  });
+  
+  
+// const server=app.listen(PORT,()=>{
+//     console.log("Servidor Express Funcionando")
+// })
 
 
-const product = new ProductManager();
+// const product = new ProductManager();
 
-const io = new Server(server);
+// const io = new Server(server);
 
 
-    io.on('connection',async socket=>{
-        console.log("cliente conectado")
-        const products = await product.getProducts();
+//     io.on('connection',async socket=>{
+//         console.log("cliente conectado")
+//         const products = await product.getProducts();
 
-        socket.emit('productos',products)
+//         socket.emit('productos',products)
 
-        socket.on('addProduct',async data=>{
-            console.log(data)
-           await product.addProduct(data)
-        })
+//         socket.on('addProduct',async data=>{
+//             console.log(data)
+//            await product.addProduct(data)
+//            io.emit('productos', products);
+//         })
 
-        socket.on('deleteProduct', async data => {
-            console.log("ID del producto a eliminar: ", data);
+//         socket.on('deleteProduct', async data => {
+//             console.log("ID del producto a eliminar: ", data);
         
-            const deletedProduct = await product.deleteProduct(data);
-            console.log("Producto eliminado: ", deletedProduct);
-          });
-    })
+//             const deletedProduct = await product.deleteProduct(data);
+//             console.log("Producto eliminado: ", deletedProduct);
+//             io.emit('productos', products);
+//           });
+//     })
--- SegundaEntrega/src/public/js/realtime.js
+++ SegundaEntrega/src/public/js/realtime.js
@@ -1,54 +1,61 @@
+
 const socket = io();
 
-let form=document.getElementById("formProduct")
+let form = document.getElementById("formProduct");
 
-form.addEventListener('submit',evt=>{
-  evt.preventDefault()
-  let title=form.elements.title.value
-  let description=form.elements.description.value
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  let title = form.elements.title.value;
+  let description = form.elements.description.value;
+  let stock = form.elements.stock.value;
+  let thumbnail = form.elements.thumbnail.value;
+  let price = form.elements.price.value;
+  let code = form.elements.code.value;
 
-  let stock=form.elements.title.value
+  socket.emit('addProduct', {
+    title,
+    description,
+    stock,
+    thumbnail,
+    price,
+    code
+  });
 
-  let thumbnail=form.elements.title.value
+  form.reset();
+});
 
-  let price=form.elements.title.value
-  let code=form.elements.title.value
+document.getElementById("delete-btn").addEventListener("click", function () {
+  console.log("borrando");
+  const deleteidinput = document.getElementById("id-prod");
+  const deleteid = parseInt(deleteidinput.value);
+  socket.emit("deleteProduct", deleteid);
+  deleteidinput.value = "";
+});
 
-  console.log(title)
-  socket.emit('addProduct',{
-    title,description,stock,thumbnail,price,code
-  })
-  form.reset()
-})
+socket.on('productos', products => {
+  console.log(products);
+  updateProductList(products);
+});
 
+socket.on('newProduct', product => {
+  updateProductList([product]);
+});
 
-document.getElementById("delete-btn").addEventListener("click",function(){
-  console.log("borrando")
-  const deleteidinput=document.getElementById("id-prod")
-  const deleteid=parseInt(deleteidinput.value);
-  socket.emit("deleteProduct",deleteid);
-  deleteidinput.value="";
-})
+// Función para actualizar la lista de productos en la página web
+function updateProductList(products) {
+  let div = document.getElementById('list-products');
+  let productos = "";
 
-
-
-
-
-
-socket.on('productos',data=>{
-  console.log(data)
-
-  let div=document.getElementById('list-products')
-  let productos=""
-  data.forEach((product)=>{
-     productos += `<div class="card" style="width: 18rem;">
-        <img src="${product.image}" class="card-img-top" alt="...">
+  products.forEach(product => {
+    productos += `<div class="card" style="width: 18rem;">
+        <img src="${product.thumbnail}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${product.title}</h5>
           <p class="card-text">${product.description}</p>
           <p class="card-text">${product.price}</p>
         </div>
-      </div>`
-  })
-  div.innerHTML=productos
-});
+      </div>`;
+  });
+
+  div.innerHTML = productos;
+}
